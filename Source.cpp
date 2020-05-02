b45ddd0807d9ea28565f8cf366a7de643c0d3a6e
Galesar <alkesproject@gmail.com>
2020-05-02 03:12:45 +0300

    v. 1.0
    Осталось оптимизировать читабельность кода. И готово

--- Source.cpp
+++ Source.cpp
@@ -35,10 +35,14 @@ public:
 	void push_front(T data);
 
 	void pop_front();
+	void pop_back();
+
 	void clear();
 
 	int get_Size() { return m_Size; }
 
+	void insert(T data, int index);
+	void remove_at(int index);
 
 };
 
@@ -124,6 +128,22 @@ void List<T>::pop_front()
 }
 
 template<typename T>
+void List<T>::pop_back()
+{
+
+	Node<T>* Temp = m_Tail;
+	if (m_Tail->pPrevious != nullptr) {
+		m_Tail = m_Tail->pPrevious;
+		m_Tail->pNext = nullptr;
+	}
+
+	delete Temp;
+
+	m_Size--;
+}
+
+
+template<typename T>
 void List<T>::clear()
 {
 	while (get_Size())
@@ -132,6 +152,99 @@ void List<T>::clear()
 	}
 }
 
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+
+	int half_size = m_Size / 2;
+
+		if (index == 0)
+		{
+			push_front(data);
+		}
+		else if (index > m_Size) {
+			push_back(data);
+		}
+		else {
+
+			if (index <= half_size) {
+				Node<T>* previous = this->m_Head;
+				for (int i = 0; i < index - 1; i++)  // встречается дважды (remove at)
+				{
+					previous = previous->pNext;
+				}
+				Node<T>* newNode = new Node<T>(data, previous->pNext, previous);
+				previous->pNext = newNode;
+				Node<T>* next = newNode->pNext;
+				next->pPrevious = newNode;
+
+				m_Size++;
+			}
+			else {
+				Node<T>* Next = this->m_Tail;
+				for (int i = m_Size; i > (index+1); i--)  // встречается дважды (remove at)
+				{
+					Next = Next->pPrevious;
+				}
+				Node<T>* newNode = new Node<T>(data, Next, Next->pPrevious);
+				Next->pPrevious = newNode;
+				Node<T>* Previous = newNode->pPrevious;
+				Previous->pNext = newNode;
+
+				m_Size++;
+		}
+	}
+}
+
+template<typename T>
+void List<T>::remove_at(int index)
+{
+	int half_size = m_Size / 2;
+
+	if (index == 0)
+	{
+		pop_front();
+	}
+	else if (index > m_Size) {
+		pop_back();
+	}
+	else {
+		if (index <= half_size) {
+			Node<T>* previous = this->m_Head;
+			for (int i = 0; i < index - 1; i++) // встречается дважды (insert)
+			{
+				previous = previous->pNext;
+			}
+			Node<T>* toDelete = previous->pNext;
+			Node<T>* Next = toDelete->pNext;
+			Next->pPrevious = previous;
+			previous->pNext = Next;
+
+			delete toDelete;
+			m_Size--;
+		}
+
+		else {
+			Node<T>* toDelete  = this->m_Tail;
+			for (int i = m_Size; i > (index + 1); i--)  // встречается дважды (remove at)
+			{
+				toDelete = toDelete->pPrevious;
+			}
+			
+			Node<T>* Previous = toDelete->pPrevious;
+			Node<T>* Next = toDelete->pNext;
+			Previous->pNext = Next;
+			Next->pPrevious = Previous;
+
+
+			delete toDelete;
+			m_Size--;
+		}
+
+	}
+}
+
+
 
 
 
@@ -139,37 +252,19 @@ int main() {
 
 	List<int> test;
 
-	int arr_size;
-	std::cin >> arr_size;
-	std::cout << std::endl;
-	for (int i = 0; i < arr_size; i++)
-	{
-		test.push_back(rand() % 10);
-	} 
+	test.push_back(0);
+	test.push_back(2);
+	test.push_back(3);
+	test.push_back(4);
+	test.push_back(5);
 
-	for (int i = 0; i < test.get_Size(); i++) {
-		std::cout << test[i] << std::endl;
-	}
 
-	test.pop_front();
-
-	std::cout << "\n\n\n";
-
-	for (int i = 0; i < test.get_Size(); i++) {
-		std::cout << test[i] << std::endl;
-	}
-
-	test.clear();
-	
-	test.get_Size();
-	if (test.get_Size() != 0) {
-		for (int i = 0; i < test.get_Size(); i++) {
-			std::cout << test[i] << std::endl;
-		}
-	}
-	else {
-		std::cout << "Array is empty";
-	}
+	test.insert(1, 1);
+	test.insert(99, 4);
+	std::cout << test[4];
+	test.remove_at(4);
+	std::cout << test.get_Size();
 
 	return 0;
 }
+
